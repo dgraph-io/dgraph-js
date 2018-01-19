2a7b40c5bcc08a022b2f5eb6a8d630b26d723672
Garvit Pahal <g10pahal@gmail.com>
2018-01-19 14:16:32 +0530

    Update install dgraph script for the nightly version

--- scripts/install_dgraph.sh
+++ scripts/install_dgraph.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$TRAVIS_TAG" ]; then
-    curl https://nightly.dgraph.io -sSf | bash
+    curl https://get.dgraph.io -sSf | bash -s nightly
 else
     curl https://get.dgraph.io -sSf | bash
 fi
