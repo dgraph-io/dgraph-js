9c288baa42b21bbb562e009bf2469d422719e9c0
Garvit Pahal <g10pahal@gmail.com>
2018-01-29 15:10:16 +0530

    Update install_dgraph.sh to only install nighly versions for testing

--- scripts/install_dgraph.sh
+++ scripts/install_dgraph.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if [ -z "$TRAVIS_TAG" ]; then
-    curl https://get.dgraph.io -sSf | bash -s nightly
-else
-    curl https://get.dgraph.io -sSf | bash
-fi
+curl https://get.dgraph.io -sSf | bash -s nightly
