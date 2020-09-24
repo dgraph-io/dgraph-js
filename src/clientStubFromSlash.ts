11790e8d46aee3b7260cb939fdb8e2e8b5144a65
Anurag Sharma <anurags92@gmail.com>
2020-09-24 15:13:18 +0530

    Make tslint happy

--- src/clientStubFromSlash.ts
+++ src/clientStubFromSlash.ts
@@ -1,28 +1,29 @@
-//clientStubFromSlash
-
 import * as grpc from "@grpc/grpc-js";
-import { DgraphClientStub } from "./clientStub";
 import * as Url from "url-parse";
+import { DgraphClientStub } from "./clientStub";
 
 const PORT = "443";
 export function clientStubFromSlashGraphQLEndpoint(
     graphqlEndpoint: string,
     apiKey: string,
 ) {
-    let url = new Url(graphqlEndpoint);
-    let urlParts = url.host.split(".");
-    let firstHalf = urlParts[0];
-    let secondHalf = urlParts.splice(1).join(".") + ":" + PORT;
-    let backenedURL = firstHalf + ".grpc." + secondHalf;
+    const url = new Url(graphqlEndpoint);
+    const urlParts = url.host.split(".");
+    const firstHalf = urlParts[0];
+    const secondHalf = `${urlParts.splice(1).join(".")}:${PORT}`;
+    const backenedURL = `${firstHalf}.grpc.${secondHalf}`;
 
     const metaCreds = grpc.credentials.createFromMetadataGenerator(
-        (_, callback) => {
+        (
+            _: Object,
+            callback: (_: undefined, metadata: grpc.Metadata) => void,
+        ) => {
             const metadata = new grpc.Metadata();
             metadata.add("authorization", apiKey);
-            callback(null, metadata);
+            callback(undefined, metadata);
         },
     );
-    let credentials = grpc.credentials.combineChannelCredentials(
+    const credentials = grpc.credentials.combineChannelCredentials(
         grpc.credentials.createSsl(),
         metaCreds,
     );
